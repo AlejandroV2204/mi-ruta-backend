46c7b9044179d1c55c564b4a503429ba83d387f5
AlejandroV2204 <91919753+AlejandroV2204@users.noreply.github.com>
2024-12-05 01:45:08 -0500

    evitar error 404

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import app from "./app.js"
 import { PORT } from "./config.js";
 
+// Ruta principal
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+  });
 app.listen(PORT, () => {
     console.log(`Server on port ${PORT}`); 
 });
